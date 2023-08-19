cfda440763273fda33255d189ac4bc421b53cf49
Nandini Nema <nandininema05@gmail.com>
2023-08-19 08:34:20 -0600

    Adding Exploring Art Forms Mini Project

---
+++ exploring_art_forms_mini_project/script.js
@@ -0,0 +1,111 @@
+
+//Ballet
+//Change the image to text that describes the art form
+//Referencing the image
+const balletImage = document.querySelector('#balletImage')
+balletImage.addEventListener('mouseenter', e => {
+  console.log('Hovered over ballet image');
+  balletImage.innerHTML = "Ballet dance developed during the Italian Renaissance, before evolving in France and Russia into a concert dance meant for public performance. This is in the form of a ballet, in which the dance is choreographed with classical music. Ballet productions vary between using elaborate costumes and staging and using minimal costuming and bare staging. Ballet is now a widespread, highly technical form of dance with many subgenres including classic, romantic, neoclassical and contemporary. With six core recognised methods: the Cecchetti method, the Bournoville method, the Vaganova method, the French School, the Royal Academy of Dance method and the Balanchine method, ballet is studied professionally at top dance schools all over the world."
+  
+});
+
+
+balletImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out ballet image');
+  balletImage.innerHTML = "<img src = 'https://cms.harlequinfloors.com/wp-content/uploads/2020/01/scottish-ballet-1024x602.jpg'/>"
+});
+
+//Hip Hop
+//Change the image to text that describes the art form
+//Referencing the image
+
+const HipHopImage = document.querySelector('#HipHopImage')
+HipHopImage.addEventListener('mouseover', e => {
+  console.log('Hovered over HipHop image');
+  HipHopImage.innerHTML = "Hip-hop dancing refers to a range of street dances that developed in relation to hip hop music and culture. Hip-hop dancing dates back to the early 1970s in New York and California, evolving out of Funk and the development of break beat. Main styles of hip-hop dancing include Breaking, Locking and Popping, with derivative styles emerging out of these including Memphis Jookin’, Turfing, Jerkin’ and Krumping. These were often popularised and made mainstream after being featured in music videos of the time. Today, hip-hop is performed in outdoor spaces, in dance studios and competitively. Unlike many competitive dance styles, hip-hop is often improvisational with dance crews challenging each other to dance battles."
+  
+});
+
+
+HipHopImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out hip hop image');
+  HipHopImage.innerHTML = "<img src = 'https://cms.harlequinfloors.com/wp-content/uploads/2020/01/20-Jahre-3..jpg'/>"
+});
+
+//Jazz
+//Change the image to text that describes the art form
+//Referencing the image
+const jazzImage = document.querySelector('#jazzImage')
+jazzImage.addEventListener('mouseover', e => {
+  console.log('Hovered over jazz image');
+  jazzImage.innerHTML = "Jazz dancing has its roots in seventeenth-century African traditions, brought to the Americas via the Atlantic slave trade as slaves continued dancing traditions in Brazil, the US and elsewhere on the continents. Known for its improvisational and dramatic body movements, the jazz dancing grew in popularity in early twentieth-century jazz clubs. Today, jazz dancing builds on African American vernacular dance styles that emerged along with Jazz music in the US. Swing, the Lindy Hop, the Shimmy and the Charleston are popular kinds of jazz dances." 
+});
+
+
+jazzImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out jazz image');
+  jazzImage.innerHTML = "<img src = 'https://cms.harlequinfloors.com/wp-content/uploads/2020/01/jumping-dancers-scaled.jpg'/>"
+});
+
+
+//Poetry
+//Change the image to text that describes the art form
+//Referencing the image
+const peotryImage = document.querySelector('#poetryImage')
+poetryImage.addEventListener('mouseover', e => {
+  console.log('Hovered over poetry image');
+  poetryImage.innerHTML = "Writing that formulates a concentrated imaginative awareness of experience in language chosen and arranged to create a specific emotional response through its meaning, sound, and rhythm. It may be distinguished from prose by its compression, frequent use of conventions of metre and rhyme, use of the line as a formal unit, heightened vocabulary, and freedom of syntax. Its emotional content is expressed through a variety of techniques, from direct description to symbolism, including the use of metaphor and simile." 
+});
+
+
+poetryImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out poetry image');
+  poetryImage.innerHTML = "<img src='https://assets.ltkcontent.com/images/9037/examples-of-poetry-genres_0066f46bde.jpg' />"
+});
+
+//Screenplay
+//Change the image to text that describes the art form
+//Referencing the image
+const screenplayImage = document.querySelector('#screenplayImage')
+screenplayImage.addEventListener('mouseover', e => {
+  console.log('Hovered over screenplay image');
+  screenplayImage.innerHTML = "A screenplay or movie script is the blueprint for any feature film, television show, or video game. Character actions, language, movement, and stage direction are all included in the scripts. The script writing format used in a movie script has its own set of industry-standard norms, which varies slightly from the scriptwriting style used in a shooting script. A shooting script is a more precisely formatted version of the pre-production and production script to transform the screenplay into a movie. Camera directions, music cues, and transitions can all be included in this version." 
+});
+
+
+screenplayImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out screenplay image');
+  screenplayImage.innerHTML = "<img src = 'https://nofilmschool.com/sites/default/files/styles/article_wide/public/screen_shot_2021-07-09_at_9.51.17_am.png?itok=ad5ykc6W'/>"
+});
+
+//Painting
+//Change the image to text that describes the art form
+//Referencing the image
+const paintingImage = document.querySelector('#paintingImage')
+paintingImage.addEventListener('mouseover', e => {
+  console.log('Hovered over painting image');
+  paintingImage.innerHTML = "The expression of ideas and emotions, with the creation of certain aesthetic qualities, in a two-dimensional visual language. The elements of this language—its shapes, lines, colours, tones, and textures—are used in various ways to produce sensations of volume, space, movement, and light on a flat surface. These elements are combined into expressive patterns in order to represent real or supernatural phenomena, to interpret a narrative theme, or to create wholly abstract visual relationships. An artist’s decision to use a particular medium, such as tempera, fresco, oil, acrylic, watercolour or other water-based paints, ink, gouache, encaustic, or casein, as well as the choice of a particular form, such as mural, easel, panel, miniature, manuscript illumination, scroll, screen or fan, panorama, or any of a variety of modern forms, is based on the sensuous qualities and the expressive possibilities and limitations of those options. The choices of the medium and the form, as well as the artist’s own technique, combine to realize a unique visual image." 
+});
+
+
+paintingImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out painting image');
+  paintingImage.innerHTML = "<img src='https://mymodernmet.com/wp/wp-content/uploads/2018/05/painting-ideas-thumbnail-jpg.jpg' />"
+});
+
+
+
+//Sculpture
+//Change the image to text that describes the art form
+//Referencing the image
+const sculptureImage = document.querySelector('#sculptureImage')
+sculptureImage.addEventListener('mouseover', e => {
+  console.log('Hovered over sculpture image');
+  sculptureImage.innerHTML = "Sculpture, an artistic form in which hard or plastic materials are worked into three-dimensional art objects. The designs may be embodied in freestanding objects, in reliefs on surfaces, or in environments ranging from tableaux to contexts that envelop the spectator. An enormous variety of media may be used, including clay, wax, stone, metal, fabric, glass, wood, plaster, rubber, and random “found” objects. Materials may be carved, modeled, molded, cast, wrought, welded, sewn, assembled, or otherwise shaped and combined." 
+});
+
+
+sculptureImage.addEventListener('mouseleave', e => {
+  console.log('Mouse Out sculpture image');
+  sculptureImage.innerHTML = "<img src='https://images.pexels.com/photos/2362469/pexels-photo-2362469.jpeg?cs=srgb&dl=pexels-judit-agusti-aranda-2362469.jpg&fm=jpg'/>"
+});
